7fe0ba58c22c24e2cc173269d7b4df18c7f93a3e
Adam Kielar <adam@adamkielar.pl>
2020-04-03 22:33:54 +0200

    Add Dog, User, UserDog models and choices

---
+++ backend/pugorugh/choices.py
@@ -0,0 +1,35 @@
+GENDER_CHOICES = (
+    ('m', 'Male'),
+    ('f', 'Female'),
+    ('u', 'Unknown'),
+)
+
+SIZE_CHOICES = (
+    ('s', 'Small'),
+    ('m', 'Medium'),
+    ('l', 'Large'),
+    ('xl', 'Extra Large'),
+    ('u', 'Unknown'),
+)
+
+STATUS_CHOICES = (
+    ('l', 'linked'),
+    ('d', 'disliked'),
+)
+
+AGE_CHOICES = (
+    ('b', 'Baby'),
+    ('y', 'Young'),
+    ('a', 'Adult'),
+    ('s', 'Senior'),
+)
+
+PEDIGREE_CHOICES = (
+    ('y', 'Yes'),
+    ('n', 'No'),
+)
+
+FUR_CHOICES = (
+    ('l', 'Long'),
+    ('s', 'Short'),
+)
--- backend/pugorugh/models.py
+++ backend/pugorugh/models.py
@@ -1,4 +1,31 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from . import choices
 
+
+class Dog(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    image_filename = models.ImageField(upload_to="images/dogs/")
+    breed = models.CharField(max_length=250)
+    age = models.IntegerField(blank=True)
+    gender = models.CharField(max_length=1, choices=choices.GENDER_CHOICES)
+    size = models.CharField(max_length=2, choices=choices.SIZE_CHOICES)
+    pedigree = models.CharField(max_length=1, choices=choices.PEDIGREE_CHOICES)
+    fur = models.CharField(max_length=1, choices=choices.FUR_CHOICES)
+
+
+class UserPref(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.CharField(max_length=1, choices=choices.AGE_CHOICES)
+    gender = models.CharField(max_length=1, choices=choices.GENDER_CHOICES)
+    size = models.CharField(max_length=2, choices=choices.SIZE_CHOICES)
+    pedigree = models.CharField(max_length=1, choices=choices.PEDIGREE_CHOICES)
+    fur = models.CharField(max_length=1, choices=choices.FUR_CHOICES)
+
+
+class UserDog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=choices.STATUS_CHOICES)
