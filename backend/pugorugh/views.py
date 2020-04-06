7ec43e40d07b49d7d2f4a4a426d2e2090f8cf43d
Adam Kielar <adam@adamkielar.pl>
2020-04-06 19:01:32 +0200

    UserPreferencesView create UserDog instance

--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -18,6 +18,7 @@ class UserRegisterView(generics.CreateAPIView):
     """
     Endpoint: /api/user/
     """
+
     permission_classes = (permissions.AllowAny,)
     model = get_user_model()
     serializer_class = serializers.UserSerializer
@@ -35,6 +36,16 @@ class UserPreferencesView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         userpref = self.get_queryset().filter(user=self.request.user).first()
+
+        dogs = models.Dog.objects.filter(
+            age_letter__in=self.request.user.userpref.age,
+            gender__in=self.request.user.userpref.gender,
+            size__in=self.request.user.userpref.size,
+            pedigree__in=self.request.user.userpref.pedigree,
+            fur__in=self.request.user.userpref.fur,
+        )
+        for dog in dogs:
+            models.UserDog.objects.create(user=self.request.user, dog=dog, status='u')
         return userpref
 
     def perform_update(self, serializer):
@@ -88,17 +99,15 @@ class UserDogStatusUpdateView(generics.UpdateAPIView):
     serializer_class = serializers.UserDogSerializer
 
     def get_object(self):
-        dog_id = self.kwargs['pk']
-        status = self.kwargs['status'][0]
+        dog_id = self.kwargs["pk"]
+        status = self.kwargs["status"][0]
         try:
             user_dog = self.queryset.get(user=self.request.user.id, dog_id=dog_id)
             user_dog.status = status
             user_dog.save()
         except self.queryset.DoesNotExist:
             user_dog = self.queryset.create(
-                user=self.request.user,
-                dog_id=dog_id,
-                status=status
+                user=self.request.user, dog_id=dog_id, status=status
             )
         return user_dog
 
@@ -117,26 +126,30 @@ class DogRetrieve(generics.RetrieveUpdateAPIView):
         status = self.kwargs.get("status")[0]
         userpref = models.UserPref.objects.get(user=self.request.user.id)
         status_dogs = self.queryset.filter(
-                age_letter__in=self.request.user.userpref.age,
-                gender__in=self.request.user.userpref.gender,
-                size__in=self.request.user.userpref.size,
-                pedigree__in=self.request.user.userpref.pedigree,
-                fur__in=self.request.user.userpref.fur
-            )
+            age_letter__in=self.request.user.userpref.age,
+            gender__in=self.request.user.userpref.gender,
+            size__in=self.request.user.userpref.size,
+            pedigree__in=self.request.user.userpref.pedigree,
+            fur__in=self.request.user.userpref.fur,
+        )
 
-        if status == 'u':
-            status_dogs = status_dogs.filter(userdog__status__exact='u', userdog__user=self.request.user.id)
-        elif status == 'l':
-            status_dogs = status_dogs.filter(userdog__status__exact='l', userdog__user=self.request.user.id)
+        if status == "u":
+            status_dogs = status_dogs.filter(
+                userdog__status__exact="u", userdog__user=self.request.user.id
+            )
+        elif status == "l":
+            status_dogs = status_dogs.filter(
+                userdog__status__exact="l", userdog__user=self.request.user.id
+            )
         else:
-           status_dogs = status_dogs.filter(userdog__status__exact='d', userdog__user=self.request.user.id)
+            status_dogs = status_dogs.filter(
+                userdog__status__exact="d", userdog__user=self.request.user.id
+            )
         return status_dogs
 
     def get_object(self):
         status_dogs = self.get_queryset()
-        next_dogs = status_dogs.filter(
-            id__gt=self.kwargs.get('pk')
-        )
+        next_dogs = status_dogs.filter(id__gt=self.kwargs.get("pk"))
         if next_dogs.exists():
             return next_dogs.first()
         if status_dogs.exists():
