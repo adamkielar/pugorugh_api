f59873936254e7d5c30a6b28b64016fc06502fba
Adam Kielar <adam@adamkielar.pl>
2020-04-08 17:47:03 +0200

    Test for user preferences

--- backend/pugorugh/tests/test_views.py
+++ backend/pugorugh/tests/test_views.py
@@ -3,14 +3,16 @@ from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.test import APIClient, APIRequestFactory, APITestCase, force_authenticate
 
 from pugorugh.models import Dog, UserPref, UserDog
+from pugorugh.views import UserRegisterView, UserPreferencesView
 
 
 class APIViewsTests(APITestCase):
     def setUp(self):
-        self.user = User.objects.create(username="adam", password="testpass",)
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create(username="adam", password="testpass")
         self.dog = Dog.objects.create(
             name="Kamzik",
             breed="Border Coli",
@@ -22,16 +24,23 @@ class APIViewsTests(APITestCase):
         )
         self.user_dog = UserDog.objects.create(user=self.user, dog=self.dog, status="l")
 
-        self.token = Token.objects.create(user=self.user)
-        self.token = Token.objects.get(user__username="adam")
-        self.client = APIClient()
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+        #self.token = Token.objects.create(user=self.user)
+        #self.token = Token.objects.get(user__username="adam")
 
-    def test_account_create_endpoint(self):
-        self.client.credentials()
-        url = "/api/user/"
-        data = {"username": "adam", "password": "testpass"}
-        response = self.client.get(reverse("register-user"))
-        self.assertEqual(response.status_code, 200)
-        #response = self.client.post(url, data, format="json")
-        #self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def test_user_register_view(self):
+        view = UserRegisterView.as_view()
+        data = {'username': 'adam2', 'password': 'testpass2'}
+        request = self.factory.post("register-user", data)
+        user = User.objects.all().first()
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_preferences_view(self):
+        user = User.objects.all().first()
+        view = UserPreferencesView.as_view()
+        request = self.factory.get("preferences")
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
